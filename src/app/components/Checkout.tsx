ae05a25530736919b726d4af83b0fed9449d66f7
DaniyalGhouri <daniyalghouri443@gmail.com>
2025-02-08 09:24:51 -0800

    final commit

--- src/app/components/Checkout.tsx
+++ src/app/components/Checkout.tsx
@@ -42,7 +42,7 @@ const CheckoutPage = ({ amount }: { amount: number }) => {
       elements,
       clientSecret,
       confirmParams: {
-        return_url: `http://localhost:3000/checkout/payment/paymentsuccess?amount=${amount}`,
+        return_url: `https://final-hackathon-quarter-2-psnw.vercel.app/checkout/payment/paymentsuccess?amount=${amount}`,
       },
     });
 
